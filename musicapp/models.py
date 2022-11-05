3c9a3b1b6f61db3c371f7385b662a4ac929bd366
Oiseh Omokheoa <oisehzeme@gmail.com>
2022-11-05 09:54:53 +0100

    api

--- musicapp/models.py
+++ musicapp/models.py
@@ -6,12 +6,20 @@ class Artiste(models.Model):
     last_name = models.CharField(max_length=50)
     age = models.CharField(max_length=50)
     
+    def __str__(self):
+        return self.first_name
+    
 class Song(models.Model):
     title = models.CharField(max_length=50)
     artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     likes = models.IntegerField(default=0)
     release_date = models.DateField()
+    def __str__(self):
+        return self.title
+
 class Lyric(models.Model):
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
     content = models.TextField()
+    def __str__(self):
+        return self.content
     
--- musicapp/views.py
+++ musicapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from .models import Artiste, Song, Lyric
+from .serializers import ArtisteSerializer, SongSerializer, LyricSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+@api_view(['GET','PUT','DELETE'])
+def musicapp_api(request):
+    if request.method == 'GET':
+        data = Song.objects.all()
+        serializer = SongSerializer(data, context={'request': request}, many=True)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    elif request.method == 'DELETE':
+        Song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
